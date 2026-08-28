223dbc3d07861c237eb43a3773380083e65e3571
davidcarusso <carusso479@gmail.com>
2026-08-27 22:23:36 -0300

    Cambio de README.md probando archivo interpretado por claudio.ia

--- 05 - Window Functions/preentrega_analisis_avanzado.sql
+++ 05 - Window Functions/preentrega_analisis_avanzado.sql
@@ -9,6 +9,7 @@ FROM entregable_05.venta AS v
 ), datos_agrupados AS 
 (
 SELECT 
+-- algunas columnas quedan sin usar ya que se esta probando las funciones. 
 	fecha
 	, EXTRACT (HOUR FROM fecha) AS hora
 	, CASE 
@@ -19,7 +20,8 @@ SELECT
 	, EXTRACT(YEAR FROM datos.fecha) AS "año"
 	, EXTRACT(MONTH FROM fecha) AS mes
 	--, CAST(EXTRACT(MONTH FROM fecha)  + EXTRACT(YEAR FROM fecha) * 100 AS VARCHAR) AS periodo_var --castear con CAST
-	, (EXTRACT(YEAR FROM fecha):: INT * 100 + EXTRACT(MONTH FROM fecha):: INT) AS periodo
+	--, (EXTRACT(YEAR FROM fecha):: INT * 100 + EXTRACT(MONTH FROM fecha):: INT) AS periodo
+	, TO_CHAR(fecha , 'YYYYMM') AS periodo
 	, nombre_producto	AS producto
 	, nombre_categoria	AS categoria
 	, cantidad 
@@ -31,10 +33,11 @@ SELECT
 	año
 ,	mes
 ,	categoria
+, 	periodo
 --
 ,	sum(cantidad::INT * precio) AS ventas
 FROM datos_agrupados
-GROUP BY año, mes, categoria
+GROUP BY año, mes, categoria, periodo
 ORDER BY año, mes, ventas
 ), consulta_final AS 
 (
@@ -43,14 +46,22 @@ SELECT
 ,	DENSE_RANK() OVER(PARTITION BY año, mes ORDER BY ventas DESC) AS ranking_categorias
 , 	SUM(ventas) OVER(PARTITION BY categoria ORDER BY año, mes) AS sum_acumulado_categoria
 --,	round(AVG(ventas) OVER(PARTITION BY categoria),2) AS promedio_categoria
+--- se inserto emoticones para probarlos. 
 , 	CASE 
-		WHEN ventas > AVG(ventas) OVER(PARTITION BY categoria)  THEN 'Exitoso'
-		ELSE 'Bajo el promedio'
+		WHEN ventas > AVG(ventas) OVER(PARTITION BY categoria)  THEN '🌞 Exitoso 🌞'
+		ELSE '🆘 Bajo el promedio 🆘'
 	END AS venta_mensual_contra_promedio
 FROM ventas_mensuales
 ORDER BY 1, 2, ventas DESC
 )
-SELECT 
-	*
+SELECT
+--- Le di otro formato a las monedas para probarlas. 
+	periodo
+,	categoria
+,	ventas :: MONEY
+,	ranking_categorias
+,	sum_acumulado_categoria :: MONEY
+,	venta_mensual_contra_promedio
 FROM consulta_final
+ORDER BY año , mes
 ;
